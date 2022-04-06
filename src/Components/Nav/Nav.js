17f502e5d61c43a6dd15163376932150d653bbe3
Colgan Meanor <colganmeanor7@gmail.com>
2022-04-06 09:56:34 -0500

    Add Nav items and flex positioning

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,18 @@
+import './Nav.css';
+
 const Nav = () => {
     return (
-        <p>This will be the nav.</p>
+        <div className='nav-bar'>
+            <div className='title-message'>
+               <p>Hi - I'm Colgan. :)</p>
+            </div>
+            <div className='nav-links'>
+                <p>About Me</p>
+                <p>Projects</p>
+                <p>Resume</p>
+                <p>Let's Connect</p>
+            </div>
+        </div>
     )
 }
 
