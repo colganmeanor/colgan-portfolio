d76f7971d2d46f4c6a9b00ee4396254a5004cd1e
Colgan Meanor <colganmeanor7@gmail.com>
2022-04-13 15:17:46 -0500

    Update Resume Link in Nav Bar to link to PDF in new page

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import './Nav.css';
+import resume from '../../Assets/colgan_resume.pdf'
 import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
@@ -10,7 +11,7 @@ const Nav = () => {
             <div className='nav-links'>
                 <NavLink to='/'>About Me</NavLink>
                 <NavLink to='/projects'>Projects</NavLink>
-                <p>Resume</p>
+                <a href={resume} target='_blank'>Resume</a>
                 <NavLink to='/contact'>Let's Connect!</NavLink>
             </div>
         </div>
