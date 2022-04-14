454f95fea1d4d885f9bc6f9ded1f5aee59f38693
Colgan Meanor <colganmeanor7@gmail.com>
2022-04-14 11:57:41 -0500

    Establish data structure for Projects in project_data.js

---
+++ src/Data/project_data.js
@@ -0,0 +1,14 @@
+export const projectData = [
+    {
+        name: '',
+        description: '',
+        hours: 0,
+        days: 0,
+        developers: 0,
+        technologies: [''],
+        deployedURL: '',
+        githubURL: '',
+        achievements: [''],
+        screenshots: ['']
+    }
+]
