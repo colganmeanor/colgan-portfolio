19543f8a90e806637dbd5e36e28f3f0afd75aadf
Colgan Meanor <colganmeanor7@gmail.com>
2022-04-14 11:17:35 -0500

    Add initial info to About component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,11 @@
-
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <p>This is the About Section</p>
+        <div className="about-section">
+            <p>I'm a front-end software developer, with former experience in technical support, marketing, and search engine optimization. I'm currently based out of New Orleans, LA, open to work opportunities here in NOLA and remote opportunities around the country.</p>
+            <p>My goal is to build a career for myself where I am challenged, where I get to use my brain to solve problems, where I can learn and foster my own growth and personal development, and where I can use my skills to potentially help make the world a better place for us to live.</p>
+            <p>When I'm not coding... I'm playing video games, catching up on the best movies and television of the past year, or out exploring NOLA with my wife and searching for the best food and drinks that it can offer.</p>
         </div>
     )
 }
