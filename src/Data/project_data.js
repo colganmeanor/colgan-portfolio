4b8f587afa4931a59d10db2da3fa0b2408edc867
Colgan Meanor <colganmeanor7@gmail.com>
2022-04-15 14:07:37 -0500

    Add project data for Crossing Companion

--- src/Data/project_data.js
+++ src/Data/project_data.js
@@ -1,15 +1,15 @@
 export const projectData = [
     {
-        name: '',
-        description: '',
-        hours: 0,
-        days: 0,
-        developers: 0,
-        technologies: [''],
-        deployedURL: '',
-        githubURL: '',
+        name: 'Crossing Companion',
+        description: 'Crossing Companion is an app built to help Animal Crossing players track currently available critters and their critter collection progress.',
+        hours: 36,
+        days: 6,
+        developers: 1,
+        technologies: ['Javascript', 'HTML5', 'CSS', 'React.js', 'Git', 'Cypress', 'Travis CI', 'Github Pages', 'ACNH-API'],
+        deployedURL: 'https://colganmeanor.github.io/Crossing-Companion/#/',
+        githubURL: 'https://github.com/colganmeanor/Crossing-Companion',
         achievements: [''],
-        contributions: '',
+        contributions: ['Everything'],
         screenshots: ['']
     },
     {
